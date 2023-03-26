788f488102d2bb4b6b834a6aa2e52d7b41c3ee48
Taxvirax <69835749+Taxvirax@users.noreply.github.com>
2023-03-26 15:56:40 +0700

    เพิ่ม font ลิงค์ bootstrap

---
+++ resources/views/layouts/bootstrap.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/font.css')}}">
+</head>
+<body>
+    
+    
+    <script src="{{asset('bootstrap/js/bootstrap.js')}}"></script>
+    <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.js')}}"></script>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.bootstrap')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
